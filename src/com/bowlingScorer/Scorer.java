01aceb486e5639acd0dfa59429e737f4a33bc038
John Furtado <jf8846@gmail.com>
2018-07-31 18:16:21 -0400

    end of 90 minutes - non-perfect game

--- src/com/bowlingScorer/Scorer.java
+++ src/com/bowlingScorer/Scorer.java
@@ -2,7 +2,7 @@ package com.bowlingScorer;
 
 public class Scorer {
 	
-	public static int[] scores = {10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10};
+	public static int[] scores = {1,4,4,5,6,4,5,5,10,0,0,1,7,3,6,4,10,0,2,8,6};
 
 	public static void main(String[] args) {
 		
@@ -18,21 +18,18 @@ public class Scorer {
 			frame= scores[index] + scores[index+1];
 			
 			// check if strike
-			if (scores[index] == 10 && index < 18) {
-		
+			if (scores[index] == 10) {
 				System.out.println("Strike");
-				result += 10 + scores[index+2] + scores[index+4];
+				result += 10 + scores[index+2] + scores[index+3];
 				System.out.println("Score: " + result + "\n");
 			}
 			// check if spare
 			else if(frame == 10) {
-				
 				System.out.println("Spare");
 				result += 10 + scores[index+2];
 				System.out.println("Score: " + result + "\n");
 			}
 			else { 
-				
 				System.out.println(frame);
 				result += frame;
 				System.out.println("Score: " + result + "\n");
